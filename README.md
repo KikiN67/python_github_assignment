175f99d95367f00da05de915d41cf45f58eb1804
KikiN67 <9cxfx8b45m@privaterelay.appleid.com>
2025-11-23 22:39:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#Water Intake Monitor
+
+This program is designed to help users track their daily water intake and see how much more they need to drink to reach their goal. The user enters the number of glasses they have consumed, and the program calculates the total ounces. It will then tells the user if they have reached their daily goal or how many more glasses of water they need. It help encourages user to keep a healthy habits and consume an appropriate amount of water each day.  
+
+## How to run
+Run this program by opening app.py in Thonny and pressing the Run Button ( Green triangle ).
+You can also run it from a terminal using:
+    
+python app.py
+or
+python3 app.py
+
+## Example Output
+How many glasses of water have you drank today? 6
+
+You have consumed 48.0 ounces of water today.
+YOU DID IT! CONGRATULATIONS! You drank 6.0 glasses of water today. You hit your water goals for today!
---
+++ app.py
@@ -0,0 +1,32 @@
+# Water Intake Monitor
+# This program is designed to help users track their daily water intake and see how much more they need to drink to reach their goal. 
+
+# Step 1: Welcome message
+print("welcome to the Water Intake Monitor!")
+
+# Step 2: Ask user for input
+glasses = input("How many glasses of water have you drank today?")
+
+# step 3: Convert input and handle errors
+try:
+    glasses = float(glasses) # Convert input to a numberic value 
+except ValueError:
+    print("Oops! Please enter a valid number.")
+    exit() # Stop the program if invalid input is entered
+
+# Step 4: Perform calculation
+ounces_per_glass = 8
+total_ounces = glasses * ounces_per_glass
+
+# step 5: Display results
+daily_goal = 40 # Recommended daily water intake in ounces
+print(f"\nYou have consumed {total_ounces} ounces of water today.")
+
+# Step 6: Final decision & message based on progress
+if total_ounces >= daily_goal:
+    print(f"YOU DID IT! CONGRATULATIONS! You drank {glasses} glasses of water today. You hit your water goal for today!")
+else:
+    # how many more ounces/glasses needed
+    ounces_needed = daily_goal - total_ounces
+    glasses_needed = ounces_needed / ounces_per_glass
+    print(f"Keep going! You only need {glasses_needed:.1f} more glasses to reach your goal. Keep working hard and drinking that water!")
